f199f08759ffd68156bd970fbb9a6a97f5eada6d
Nisarg Panchal <nisarg1184@gmail.com>
2019-04-16 01:29:19 +1000

    Security, WIP

--- src/main/java/com/nisarg/security/WebSecurityConfiguration.java
+++ src/main/java/com/nisarg/security/WebSecurityConfiguration.java
@@ -2,21 +2,27 @@ package com.nisarg.security;
 
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
-    @Bean
-    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-        return new InMemoryUserDetailsManager(User.withDefaultPasswordEncoder().username("user").password("password")
-                .authorities("USER").build(),
-                User.withDefaultPasswordEncoder().username("admin").password("admin")
-                        .authorities("ADMIN", "ACTUATOR", "USER").build());
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user")
+                .password("password")
+                .roles("USER", "ACTUATOR")
+                .and()
+                .withUser("admin")
+                .password("admin")
+                .roles("USER", "ADMIN", "ACTUATOR");
     }
 
     @Override
@@ -26,7 +32,6 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ACTUATOR")
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                 .antMatchers("/admin").hasRole("ADMIN")
-                .antMatchers("/h2-console**").permitAll()
                 .antMatchers("/**").permitAll()
                 .and().httpBasic();
     }
